a6fde470397090e23589859a725f1516e93f7a9e
xuxin <827556535>
2021-03-17 18:02:55 +0800

    add:添加帮助类

--- FWDemo/src/FW.Common/Security/Crypto.cs
+++ FWDemo/src/FW.Common/Security/Crypto.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace FW.Common.Extensions
+namespace FW.Common.Security
 {
     public static class Crypto
     {
